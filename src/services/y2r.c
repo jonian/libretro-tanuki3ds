e43a81b70bf3f5608db5c2233bd370ed4ecbd932
burhanr13 <burhan.rajgara@gmail.com>
2025-02-11 20:10:43 -0500

    tired of y2r spam

--- src/services/y2r.c
+++ src/services/y2r.c
@@ -51,7 +51,7 @@ DECL_PORT(y2r) {
             cmdbuf[1] = 0;
             break;
         default:
-            lwarn("unknown command 0x%04x (%x,%x,%x,%x,%x)", cmd.command,
+            linfo("unknown command 0x%04x (%x,%x,%x,%x,%x)", cmd.command,
                   cmdbuf[1], cmdbuf[2], cmdbuf[3], cmdbuf[4], cmdbuf[5]);
             cmdbuf[0] = IPCHDR(1, 0);
             cmdbuf[1] = 0;
